bccbd26188c82d5e3601d574a8aaa7198eee68df
victor-igor <victor_igor7@hotmail.com>
2026-05-09 10:58:59 -0300

    feat: wire AppShell with dynamic sidebar margin, remove navLinks.ts

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,18 +1,26 @@
 import { Outlet } from 'react-router-dom'
+import { cn } from '@/lib/utils'
 import { TopBar } from './TopBar'
 import { Sidebar } from './Sidebar'
-import { MobileDrawer } from './MobileDrawer'
 import { BottomNav } from './BottomNav'
+import { useSidebar } from '@/hooks/useSidebar'
 
 export function AppShell() {
+  const { isExpanded } = useSidebar()
+
   return (
     <div className="min-h-screen bg-neutral-50">
       <TopBar />
       <Sidebar />
-      <MobileDrawer />
 
-      <main className="pt-14 pb-16 md:pb-0 md:ml-[64px] lg:ml-[240px] min-h-screen">
-        <div className="p-4 md:p-6">
+      <main
+        className={cn(
+          'pt-14 pb-20 md:pb-6 min-h-screen transition-all duration-200',
+          'md:ml-[64px]',
+          isExpanded ? 'lg:ml-[240px]' : 'lg:ml-[64px]',
+        )}
+      >
+        <div className="p-4 md:p-6 lg:p-8 max-w-7xl mx-auto">
           <Outlet />
         </div>
       </main>
