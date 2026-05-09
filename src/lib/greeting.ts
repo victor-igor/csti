6bfaf32689035dccdc8c16f4ec0536f48836587d
victor-igor <victor_igor7@hotmail.com>
2026-05-08 22:22:24 -0300

    Revert "feat: add dropdown-menu component and getGreeting utility"
    This reverts commit bd2295e378ce1643ce0cb80356390689bb5d028d.

--- src/lib/greeting.ts
+++
@@ -1,6 +0,0 @@
-export function getGreeting(name: string): string {
-  const hour = new Date().getHours()
-  const period = hour < 12 ? 'Bom dia' : hour < 18 ? 'Boa tarde' : 'Boa noite'
-  const firstName = name.split(' ')[0]
-  return `${period}, ${firstName}!`
-}
